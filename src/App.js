985e1e3459863f5f79afe6e10214b7518ea0cdcb
Luqman Hassan <reallookmanlook@gmail.com>
2021-11-13 20:31:13 -0800

    Added more elements

--- src/App.js
+++ src/App.js
@@ -1,52 +1,73 @@
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
-  const clickme = () => {
-    console.log('yo');
-    fetch('https://api.coinpaprika.com/v1/coins', {mode: 'cors'})
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (response) {
-        console.log(response);
-        formatdata(response);
-      });
-  };
+  let cry = [];
+  const [cryptos, setCryptos] = useState([]);
+
+  fetch(
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false',
+    {mode: 'cors'}
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response);
+      formatdata(response);
+    });
+
   const formatdata = (info) => {
     for (let i = 0; i < 100; i++) {
-      let card = document.createElement('div');
-      card.id = 'example';
-      let data1 = document.createElement('span');
-      let data2 = document.createElement('span');
-      let data3 = document.createElement('span');
-
-      let data1text = document.createTextNode(info[0].name);
-      let data2text = document.createTextNode(info[0].rank);
-      let data3text = document.createTextNode(info[0].symbol);
-
-      data1.appendChild(data1text);
-      data2.appendChild(data2text);
-      data3.appendChild(data3text);
-
-      card.appendChild(data1);
-      card.appendChild(data2);
-      card.appendChild(data3);
-
-      document.getElementById('data').appendChild(card);
+      cry.push(info[i]);
     }
+    setCryptos(cry);
   };
 
   return (
     <div className="App">
-      <h1>Crypto MarketCap Rankings</h1>
-      <h2>Top 100</h2>
-      <div id="datapoint"></div>
-      <button onClick={clickme}>CLick Here</button>
+      <div>
+        <h1>Cryptocurrency Rankings</h1>
+      </div>
+      <div>
+        <button>Market Cap</button>
+        <button>Trending</button>
+        <input placeholder="SEARCH" />
+      </div>
+      <div>
+        <button>Mode</button>
+        <button>Fav</button>
+        <span>Data updates every 5 min.</span>
+      </div>
+
+      <div>
+        <span>#</span>
+        <span>Coin</span>
+        <span>Price</span>
+        <span>24h</span>
+        <span>Mkt Cap</span>
+      </div>
+      {cryptos.length > 0 &&
+        cryptos.map((item, index) => {
+          return (
+            <div key={index}>
+              <span>{item.market_cap_rank}</span>
+              <img src={item.image} alt="logo" />
+              <span>{item.name}</span>
+              <span>{item.symbol}</span>
+              <span>{item.current_price}</span>
+              <span>{item.market_cap_change_percentage_24h}</span>
+              <span>{item.market_cap}</span>
+            </div>
+          );
+        })}
+      <div>
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+      </div>
     </div>
   );
 }
 
-// 007a19b6-f0c3-4552-9343-ff51882faefa
-// https://api.coinpaprika.com/v1/coins
 export default App;
-// https://api.coinpaprika.com/v1/coins
